5aa4a3e6245eb88c43b827ed347a63eea5f90736
Mahesh Jarati <mahesh.jarati@ikcontech.com>
2023-12-13 19:26:23 +0530

    Initial Implementation

---
+++ src/main/java/com/ikn/rms/department/RMSDepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ikn.rms.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RMSDepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RMSDepartmentServiceApplication.class, args);
+	}
+
+}
